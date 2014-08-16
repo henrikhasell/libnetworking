ffaeea8b66b50b70e6f2cc886cecdf7d90dff363
Henrik Hasell <henrikhasell@gmail.com>
2014-08-16 10:07:19 +0000

    Added comments to networking.h

--- include/networking.h
+++ include/networking.h
@@ -19,21 +19,33 @@
 extern "C" {
 #endif
 
+/* Include the appropriate headers for
+both C and C++
+Standard types: uint8_t, uint32_t */
 #ifdef __cplusplus
 #   include <cstdint>
 #else
 #   include <stdint.h>
 #endif
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* This function initialises the networking library.
+It must be called before any other function in the
+library is used. */
 int NETWORKING_API_ENTRY Networking_Initialise();
-
+/* This function frees resources used by the library.
+It should be called when the library is no longer
+required by the application. */
 int NETWORKING_API_ENTRY Networking_Cleanup();
-
+/* These functions are used to rearrange multi-byte
+data from host to network byte order and back.
+HTONL(aabbccdd) = ddccbbaa */
 uint32_t NETWORKING_API_ENTRY HTONL(uint32_t data);
 uint32_t NETWORKING_API_ENTRY NTOHL(uint32_t data);
 uint16_t NETWORKING_API_ENTRY HTONS(uint16_t data);
 uint16_t NETWORKING_API_ENTRY NTOHS(uint16_t data);
-
 #ifdef __cplusplus
 }
 #endif
