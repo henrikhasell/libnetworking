92f7f95ea079e317c77ff1b9159799199b1af09d
Henrik Hasell <henrikhasell@gmail.com>
2014-08-16 12:23:23 +0100

    Added comments.

--- include/packet.h
+++ include/packet.h
@@ -2,19 +2,15 @@
 #define PACKET_H
 
 #ifdef __cplusplus
-#	include <cstdint>
-#	include <cstring>
+#	include <cstdint> /* uint8_t, unt32_t */
+#	include <cstring> /* size_t */
 #else
-#	include <stdint.h>
-#	include <string.h>
+#	include <stdint.h> /* uint8_t, uint32_t */
+#	include <string.h> /* size_t */
 #endif
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include "networking.h"
-#include "address.h"
+#include "networking.h" /* entry point */
+#include "address.h" /* AddressInfo */
 
 #define PACKET_DEFAULT_PROTOCOL 0xdeadbeef
 
@@ -22,35 +18,53 @@ extern "C" {
 
 #define PACKET_HEADER_SIZE sizeof(uint32_t)*2 + sizeof(uint8_t) //72 bits (9 bytes)
 
-#define PACKET_TYPE_STREAM    0x00000001
-#define PACKET_TYPE_HEARTBEAT 0x00000002
-#define PACKET_TYPE_IMPORTANT 0x00000003
-#define PACKET_TYPE_RESPONSE  0x00000004
+#define PACKET_TYPE_STREAM    0x00000001 /* Unimportant data. */
+#define PACKET_TYPE_HEARTBEAT 0x00000002 /* To maintain a connection. */
+#define PACKET_TYPE_IMPORTANT 0x00000003 /* Data that needs a response. */
+#define PACKET_TYPE_RESPONSE  0x00000004 /* For arrival of important data. */
 
+/* This struct represents a buffered UDP
+packet. It contains one field to avoid
+padding. */
 typedef struct Packet{
 	 uint8_t data[PACKET_SIZE];
 }Packet;
-/*
-typedef struct PacketData{
-    uint8_t data[PACKET_SIZE - PACKET_HEADER_SIZE]; // 184 bits (23 bytes)
-}PacketData;
-*/
-void NETWORKING_API_ENTRY Packet_WriteProtocol(Packet *packet, uint32_t protocol);
-void NETWORKING_API_ENTRY Packet_WriteType(Packet *packet, uint8_t type);
-void NETWORKING_API_ENTRY Packet_WriteNumber(Packet *packet, uint32_t number);
-void NETWORKING_API_ENTRY Packet_WriteData(Packet *packet, void *data, size_t size);
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* Writes a value to the first 32 bits of the packet. The value should
+match PACKET_DEFAULT_PROTOCOL. Any packet that does not have a header
+matching PACKET_DEFAULT_PROTOCOL is automatically discarded on arrival. */
+void NETWORKING_API_ENTRY Packet_WriteProtocol(Packet *packet, uint32_t protocol);
+/* Writes the specified value into the packet header. The value should
+match one of the following enumerations: PACKET_TYPE_STREAM, PACKET_TYPE_HEARTBEAT,
+PACKET_TYPE_IMPORTANT and PACKET_TYPE_RESPONSE */
+void NETWORKING_API_ENTRY Packet_WriteType(Packet *packet, uint8_t type);
+/* Writes the specified value to the packet header. This value is intended
+to check that packets arrive in sequence and should be used as such. */ 
+void NETWORKING_API_ENTRY Packet_WriteNumber(Packet *packet, uint32_t number);
+/* Attempts to write the data into the packet's body. If the body is too
+small, the function will write as many bytes as possible. */
+void NETWORKING_API_ENTRY Packet_WriteData(Packet *packet, void *data, size_t size);
+/* This group of functions is used to read data from the packet header.
+These functions correspond to the ones above. */
 uint32_t NETWORKING_API_ENTRY Packet_ReadProtocol(Packet *packet);
 uint32_t NETWORKING_API_ENTRY Packet_ReadNumber(Packet *packet);
 uint8_t NETWORKING_API_ENTRY Packet_ReadType(Packet *packet);
 void NETWORKING_API_ENTRY *Packet_ReadData(Packet *packet);
-
+/* These functions are used to send/receive data from sockets.
+On UNIX, these can be any file descriptor. On Windows, these
+are WinSock sockets. */
 int NETWORKING_API_ENTRY Packet_Recv(Packet *packet, int file_descriptor);
 int NETWORKING_API_ENTRY Packet_Send(Packet *packet, int file_descriptor);
-
+/* This function is the same as it's above counterpart, except it stores
+the sender's address in an AddressInfo struct. This allows the user
+to send packets back. */
 int NETWORKING_API_ENTRY Packet_RecvFrom(Packet *packet, int file_descriptor, AddressInfo *address);
+/* This function sends a packet to the specified
+address, via the specified file descriptor. */
 int NETWORKING_API_ENTRY Packet_SendTo(Packet *packet, int file_descriptor, AddressInfo *address);
-
 #ifdef __cplusplus
 }
 #endif
